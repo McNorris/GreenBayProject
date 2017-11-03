401c0deeda09a8dd51f3d0f04dd7da3eb2d9449b
McNorris <33072894+McNorris@users.noreply.github.com>
2017-11-03 04:48:51 -0500

    Add files via upload

---
+++ Green Bay Project/script.js
@@ -0,0 +1,19 @@
+function rightAnswer()
+{
+alert("Damn Right!");
+}
+
+function vikingsAnswer()
+{
+alert("Get the heck off my page!");
+}
+
+function bearsAnswer()
+{
+alert("Really? You can't be serious...")
+}
+
+function lionsAnswer()
+{
+alert("Lets be real, when was the last time that happened?!?")
+}
